758e0af59cfabefd54e5a1e4850b70bca97df1c0
n41r0j <jorian@outlook.com>
2017-01-30 17:30:34 +0100

    Improvement

--- src/main/java/lnu/models/book.java
+++ src/main/java/lnu/models/book.java
@@ -3,9 +3,11 @@ package lnu.models;
 
 public class book {
     public String title;
+    public String author;
 
-    public book(String title) {
+    public book(String title, String author) {
         this.title = title;
+        this.author = author;
 
     }
 }
